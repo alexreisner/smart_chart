b6ee396c9dd4995918e392548c6bf3e354d972f3
alexreisner <alex@alexreisner.com>
2009-12-14 17:09:50 -0500

    Add spacing and y-min/max to bar chart sparkline example.

--- examples/bar_chart_sparkline.rb
+++ examples/bar_chart_sparkline.rb
@@ -7,13 +7,17 @@
 $:.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'smart_chart'
 
-width  = 40
+width  = 240
 height = 20
 
 chart = SmartChart::Bar.new(
-  :width  => width,
-  :height => height,
-  :data   => [4,60,80,50,10]
+  :width      => width,
+  :height     => height,
+  :y_min      => 0,
+  :y_max      => 100,
+  :bar_width  => 3,
+  :bar_space  => 1,
+  :data       => [38,60,68,78,52,50,19,23,33,49,38,39,12,18]
 )
 
 puts "<div style=\"width:#{width-2}px; height:#{height-1}px; overflow:hidden;\">"
